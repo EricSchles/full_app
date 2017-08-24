2af3800090c5eadf42287eb520628f431b0388ea
Eric Schles <ericschles@gmail.com>
2017-08-24 10:55:37 -0400

    changed to new backend name, for some reason

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ from celery.schedules import crontab
 
 
 def make_celery(app):
-    celery = Celery(app.name, backend=app.config['CELERY_RESULT_BACKEND'],
+    celery = Celery(app.name, backend=app.config['result_backend'],
                     broker=app.config['CELERY_BROKER_URL'])
     celery.conf.update(app.config)
     TaskBase = celery.Task
@@ -34,13 +34,13 @@ def ensure_upload_folder():
 
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+#app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 # the below line is for local development only
-#app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://eric_s:1234@localhost/vc_db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://eric_s:1234@localhost/vc_db"
 redis_url = get_redis_url()
 app.config.update(
     CELERY_BROKER_URL=redis_url,
-    CELERY_RESULT_BACKEND=redis_url
+    result_backend=redis_url
 )
 
 db = SQLAlchemy(app)
